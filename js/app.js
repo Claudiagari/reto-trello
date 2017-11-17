ee81c37f8bb65470471c058928c019b421c87634
Claudiagari <claudia.garibotto@gmail.com>
2017-11-17 15:07:09 -0500

    version 0.0.1

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 window.addEventListener('load', function() {
   var btn = document.getElementById('btn');
+  var addList = document.getElementById('addList');
   btn.addEventListener('click', function() {
-   btn.style.display='none'
+    btn.classList.add('hide');
+    addList.classList.add('show');
   });
 });
 
