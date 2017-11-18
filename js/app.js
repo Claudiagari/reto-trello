c55aeca9c15a6f103d1a6856d42eeab57865c5fa
Claudiagari <claudia.garibotto@gmail.com>
2017-11-18 13:19:22 -0500

    version 0.0.2

--- js/app.js
+++ js/app.js
@@ -1,9 +1,29 @@
 window.addEventListener('load', function() {
   var btn = document.getElementById('btn');
   var addList = document.getElementById('addList');
+  var inputList = document.getElementById('inputList');
+  var save1 = document.getElementById('save1');
+  var close1 = document.getElementById('close1')
   btn.addEventListener('click', function() {
     btn.classList.add('hide');
     addList.classList.add('show');
   });
+  save1.addEventListener('click', function(event) {
+    addList.classList.add('hide');
+    var headerList = document.createElement('div');
+    var addcard = document.createElement('a');
+    addcard.setAttribute('href', '#');
+    addcard.innerHTML = 'Añadir una tarjeta...';
+    addcard.classList.add('addcard');
+    var textheader = inputList.value;
+    headerList.innerHTML = textheader;
+    headerList.classList.add('headerList');
+    document.body.appendChild(headerList);
+    document.body.appendChild(addcard);
+    addList.classList.add('left');
+    event.preventDefault();
+  });
+
+
 });
 
