780545a14832dba0f0cfd06e4f20f4d63807ad0f
Claudiagari <claudia.garibotto@gmail.com>
2017-11-20 09:53:31 -0500

    añadiendo Readme y comentarios

---
+++ README.md
@@ -0,0 +1,10 @@
+# Reto Trello
+Recrear la pagina de Trello, mediante javascript darle la funcionalidad que se muestra en la imagen.
+
+![modelo](https://fotos.subefotos.com/8c1d8e9ff52724fa5d88593225d22438o.gif)
+
+## Herramientas 
+* Visual Studio Code.
+* Html.
+* Css.
+* Javascript.
--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
+// Iniciando el archivo despues que cargue la pagina
 window.addEventListener('load', function() {
   var btn = document.getElementById('btn');
   var addList = document.getElementById('addList');
   var inputList = document.getElementById('inputList');
   var save1 = document.getElementById('save1');
   var close1 = document.getElementById('close1');
+  // Al hacer click esconder el boton de añadir lista y mostar el formulario
   btn.addEventListener('click', function() {
     btn.classList.add('hide');
     addList.classList.add('show');
   });
+  // LLamar al boton y al hacer click crear un div con el contenido y un nuevo link para agregar tarjeta
   save1.addEventListener('click', function(event) {
     addList.classList.add('hide');
     var headerList = document.createElement('div');
@@ -24,6 +27,7 @@ window.addEventListener('load', function() {
     inputList.value = '';
     addList.classList.add('left');
     event.preventDefault();
+    // Al hacer click al link de añadir tarjeta se crea un textarea con un boton de añadir
     addcard.addEventListener('click', function() {
       addcard.classList.toggle('addcard');
       addcard.classList.add('hide');
@@ -37,12 +41,15 @@ window.addEventListener('load', function() {
       divTextarea.appendChild(add);
       divTextarea.classList.add('newdiv');
       document.body.appendChild(divTextarea);
+      // Se le da focus al textarea
       textarea.focus();
+      // Al darle click al boton añadir se crea un div con el valor del textarea y se muestra el textarea con el boton añadir
       add.addEventListener('click', function() {
         document.body.removeChild(divTextarea);
         var card = document.createElement('div');
         var textcard = textarea.value;
         card.textContent = textcard;
+        // Se da un valor vacio para mostrar denuevo
         textarea.value = '';
         card.classList.add('card');
         document.body.appendChild(card);
