e4456ee7b343f3f8ef568a13903fa47c800be604
Claudiagari <claudia.garibotto@gmail.com>
2017-11-21 00:10:40 -0500

    version 0.0.6 mejorado

--- js/app.js
+++ js/app.js
@@ -5,14 +5,17 @@ window.addEventListener('load', function() {
   var inputList = document.getElementById('inputList');
   var save1 = document.getElementById('save1');
   var close1 = document.getElementById('close1');
+  var conteiner = document.getElementById('section');
+  addList.classList.add('hide');
   // Al hacer click esconder el boton de añadir lista y mostar el formulario
   btn.addEventListener('click', function() {
     btn.classList.add('hide');
-    addList.classList.add('show');
+    addList.classList.toggle('hide');
+    addList.classList.toggle('show');
   });
   // LLamar al boton y al hacer click crear un div con el contenido y un nuevo link para agregar tarjeta
   save1.addEventListener('click', function(event) {
-    addList.classList.add('hide');
+    conteiner.removeChild(addList);
     var headerList = document.createElement('div');
     var addcard = document.createElement('a');
     addcard.setAttribute('href', '#');
@@ -22,10 +25,10 @@ window.addEventListener('load', function() {
     var textheader = inputList.value;
     headerList.innerHTML = textheader;
     headerList.classList.add('headerList');
-    document.body.appendChild(headerList);
-    document.body.appendChild(addcard);
+    conteiner.appendChild(headerList);
+    conteiner.appendChild(addList)
     inputList.value = '';
-    addList.classList.add('left');
+    headerList.appendChild(addcard);
     event.preventDefault();
     // Al hacer click al link de añadir tarjeta se crea un textarea con un boton de añadir
     addcard.addEventListener('click', function() {
@@ -40,20 +43,18 @@ window.addEventListener('load', function() {
       divTextarea.appendChild(textarea);
       divTextarea.appendChild(add);
       divTextarea.classList.add('newdiv');
-      document.body.appendChild(divTextarea);
+      headerList.appendChild(divTextarea);
       // Se le da focus al textarea
       textarea.focus();
       // Al darle click al boton añadir se crea un div con el valor del textarea y se muestra el textarea con el boton añadir
       add.addEventListener('click', function() {
-        document.body.removeChild(divTextarea);
         var card = document.createElement('div');
         var textcard = textarea.value;
         card.textContent = textcard;
         // Se da un valor vacio para mostrar denuevo
         textarea.value = '';
         card.classList.add('card');
-        document.body.appendChild(card);
-        document.body.appendChild(divTextarea);
+        headerList.insertBefore(card, divTextarea);
         textarea.focus();
       });
     });
